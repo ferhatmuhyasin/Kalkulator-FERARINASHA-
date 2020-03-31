4db6e128d0e5c8763a069eef2409c55f6a48f009
ferhatmuhyasin <62926532+ferhatmuhyasin@users.noreply.github.com>
2020-03-31 13:09:42 +0700

    Create method-perkalian.cpp

---
+++ method-perkalian.cpp
@@ -0,0 +1,13 @@
+void_tambah(){  
+    int a, b;
+    
+    cout<<"Masukkan Bilangan Pertama : ";
+    cin>>a;
+    cout<<"Masukkan Bilangan kedua : ";
+    cin>>b;
+    cout<<"Hasil Perkalian = "<<a * b;
+}
+
+main(){
+    void_kali();
+}
